cd66f6db9a937b8f2196c0cfdb847c250b109303
nikita.pankratov <nickpank112@yandex.ru>
2024-07-15 14:25:59 +0300

    replace common middlewares

--- cmd/service-a/http/router.go
+++ cmd/service-a/http/router.go
@@ -6,15 +6,15 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/iamsorryprincess/project-layout/cmd/service-a/http/handler"
 	"github.com/iamsorryprincess/project-layout/cmd/service-a/http/middleware"
-	httppkg "github.com/iamsorryprincess/project-layout/internal/pkg/http"
+	middlewarecommon "github.com/iamsorryprincess/project-layout/internal/pkg/http/middleware"
 	"github.com/iamsorryprincess/project-layout/internal/pkg/log"
 )
 
 func NewRouter(dataProvider handler.DataProvider, sessionProvider handler.SessionProvider, logger log.Logger) http.Handler {
 	router := chi.NewRouter()
 
-	router.Use(httppkg.Recovery(logger))
-	router.Use(httppkg.Cors)
+	router.Use(middlewarecommon.Recovery(logger))
+	router.Use(middlewarecommon.Cors)
 	router.Use(middleware.Test(logger))
 
 	h := handler.NewHandler(logger, sessionProvider, dataProvider)
